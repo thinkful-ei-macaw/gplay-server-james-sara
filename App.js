0659732f28f371ad64acc169458595740af9fd13
James Souksavanh Moua <Smoua1558@gmail.com>
2020-03-24 15:51:29 -0400

    implement validation authorization and env variable

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
+require('dotenv').config();
+
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const helmet = require('helmet');
+
 
 const data = require('./movies-data-small.json');
+const API_TOKEN = process.env.API_TOKEN;
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(validateAuthorization);
+app.use(helmet());
 app.use(cors());
+
+console.log(API_TOKEN);
 
 function validateAuthorization(req, res, next) {
   const authValue = req.get('Authorization');
@@ -23,9 +31,32 @@ function validateAuthorization(req, res, next) {
   const token = authValue.split(' ')[1];
 
   if (token !== API_TOKEN) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+
+    return res.status(401).json({ error: 'Invalid credentials' });
+  }
+  next();
+}
+
+function handleTypes(req, res) {
+  const { movie } = req.query;
+
+  if(!movie) {
+    return res  
+      .send(data);
+  }
+
+  if (movie) {
+    if(!['movie'].includes(movie)) {
+      return res  
+        .status(400)
+        .send('Genre must be one of genre, country or avg_vote');
+    }
   }
 }
 
+app.get( '/movie', validateAuthorization, handleTypes);
 
-app.get( '/movie', req, res);
+
+app.listen(8080, () => {
+  console.log('Server started on PORT 8080');
+});
